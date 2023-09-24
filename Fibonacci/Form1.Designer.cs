1bd07af66f805e5a63ae6351745fca6e9a57db7c
Miguel Rodriguez <m.angel.desarrolladorweb@gmail.com>
2023-09-23 18:08:37 -0600

    Agregar archivos de proyecto.

---
+++ Fibonacci/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Fibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.Iniciar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fibonacci";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblSerie);
+            this.panel1.Controls.Add(this.Iniciar);
+            this.panel1.Controls.Add(this.txtNumero);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 312);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblSerie
+            // 
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSerie.Location = new System.Drawing.Point(22, 216);
+            this.lblSerie.MaximumSize = new System.Drawing.Size(257, 0);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblSerie.Size = new System.Drawing.Size(0, 20);
+            this.lblSerie.TabIndex = 3;
+            // 
+            // Iniciar
+            // 
+            this.Iniciar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Iniciar.Location = new System.Drawing.Point(110, 162);
+            this.Iniciar.Name = "Iniciar";
+            this.Iniciar.Size = new System.Drawing.Size(86, 30);
+            this.Iniciar.TabIndex = 2;
+            this.Iniciar.Text = "Iniciar";
+            this.Iniciar.UseVisualStyleBackColor = true;
+            this.Iniciar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.ForeColor = System.Drawing.Color.Gray;
+            this.txtNumero.Location = new System.Drawing.Point(22, 111);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(196, 26);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.Text = "Cantidad";
+            this.txtNumero.Enter += new System.EventHandler(this.txtNumero_Enter);
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            this.txtNumero.Leave += new System.EventHandler(this.txtNumero_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Cantidad de numeros que desea imprimir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 312);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serie de Fibonacci";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Iniciar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblSerie;
+    }
+}
+
---
+++ Fibonacci/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static int SerieFibonacci(int numero)
+        {
+            if(numero<=1)
+            {
+                return numero;
+            }
+            else
+            {
+                return SerieFibonacci(numero - 1) + SerieFibonacci(numero - 2);
+            }
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            if(txtNumero.Text == String.Empty || txtNumero.Text == "Cantidad")
+            {
+                MessageBox.Show("Agregue una cantidad para iniciar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else 
+            { 
+                int n = Int16.Parse(txtNumero.Text);
+                txtNumero.Text = "Cantidad";
+                txtNumero.ForeColor = Color.Gray;
+                lblSerie.Text = "";
+                for (int i = 0; i < n; i++)
+                {
+                    lblSerie.Text += (SerieFibonacci(i) + "  ").ToString();
+                }
+            }
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNumero_Enter(object sender, EventArgs e)
+        {
+            if (txtNumero.Text == "Cantidad")
+            {
+                txtNumero.Text = "";
+                txtNumero.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtNumero_Leave(object sender, EventArgs e)
+        {
+            if (txtNumero.Text == "")
+            {
+                txtNumero.Text = "Cantidad";
+                txtNumero.ForeColor = Color.Gray;
+            }
+        }
+    }
+}
